7877312fab72b6ec798dbb75ed59d5261f3a7f0a
Michael Desa <mjdesa@gmail.com>
2012-08-20 18:15:49 -0700

    Initial commit

--- src/turdAlert/core.clj
+++ src/turdAlert/core.clj
@@ -1,6 +1,11 @@
-(ns turdAlert.core)
+(ns turdAlert.core
+  (:use     [ring.adapter.jetty :only [run-jetty]])
+  (:require [net.cgrand.enlive-html :as html]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defn -main [port]
+  (run-jetty app {:port (Integer. port)}))
