c5bc6d7ca6a455967a927ac9d7f29a97137de278
Michael Desa <mjdesa@gmail.com>
2012-08-22 00:38:12 -0700

    server running on my comp, should be able to add routes

--- src/turdAlert/core.clj
+++ src/turdAlert/core.clj
@@ -1,11 +1,85 @@
 (ns turdAlert.core
-  (:use     [ring.adapter.jetty :only [run-jetty]])
-  (:require [net.cgrand.enlive-html :as html]))
+   (:require [net.cgrand.enlive-html :as html])
+   (:use [net.cgrand.moustache :only [app]]
+         [ring.adapter.jetty :only [run-jetty]]
+        [ring.util.response :only [response file-response]]
+        [ring.middleware.reload :only [wrap-reload]]
+        [ring.middleware.file :only [wrap-file]]
+        [ring.middleware.stacktrace :only [wrap-stacktrace]]))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, world"})
+
+(defn pwd
+  "Returns current working directory as a String.  (Like UNIX 'pwd'.)
+  Note: In Java, you cannot change the current working directory."
+  []
+  (System/getProperty "user.dir"))
+
+
+(def *webdir* (str (pwd) "/src/turdAlert/"))
+
+(defn render [t]
+  (apply str t))
+
+(defn render-snippet [s]
+  (apply str (html/emit* s)))
+
+(def render-to-response
+     (comp response render))
+
+(defn page-not-found [req]
+  {:status 404
+   :headers {"Content-type" "text/html"}
+   :body "Page Not Found"})
+
+(defn render-request [afn & args]
+  (fn [req] (render-to-response (apply afn args))))
+
+(defn serve-file [filename]
+  (file-response
+   {:root *webdir*
+    :index-files? true
+    :html-files? true}))
+
+(defn app* [app & {:keys [port] :or {port 8080}}]
+  (let [nses (if-let [m (meta app)]
+               [(-> (:ns (meta app)) str symbol)]
+               [])]
+    (run-jetty
+     (-> app
+         (wrap-file *webdir*)
+         (wrap-reload nses)
+         (wrap-stacktrace))
+     {:port (Integer. port)})))
+
+(defmacro serve-app [app]
+  `(app* (var ~app)))
+
+(defmulti parse-int type)
+(defmethod parse-int java.lang.Integer [n] n)
+(defmethod parse-int java.lang.String [s] (Integer/parseInt s))
+
+(defmacro maybe-substitute
+  ([expr] `(if-let [x# ~expr] (html/substitute x#) identity))
+  ([expr & exprs] `(maybe-substitute (or ~expr ~@exprs))))
+
+(defmacro maybe-content
+  ([expr] `(if-let [x# ~expr] (html/content x#) identity))
+  ([expr & exprs] `(maybe-content (or ~expr ~@exprs))))
+
+(defn pluralize [astr n]
+  (if (= n 1)
+    (str astr)
+    (str astr "s")))
+
+
+
+(def routes
+  (app
+      [""]  (fn [req] (render-to-response "Hello, there"))
+      [&]   page-not-found))
 
 (defn -main [port]
-  (run-jetty app {:port (Integer. port)}))
+  (serve-app routes))
+
+(defonce *server* (serve-app routes))
+
