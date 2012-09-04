0a9939293b30251f7edcf61dbfd680c5e37a44c0
Michael Desa <mjdesa@gmail.com>
2012-09-03 23:45:29 -0700

    added get posts and stuff

--- src/turdAlert/gets.clj
+++ src/turdAlert/gets.clj
@@ -5,12 +5,6 @@
 
 (defn encrypt-pw [pw] 1000)
 
-(defn date-format [date]
-  (let [temp (clojure.string/split date #"\s")
-        day (temp 0)
-        time (temp 1)]
-    (str (day-format day) " " (time-format time))))
-
 (defn day-format [day]
   (let [dray (clojure.string/split day #"\W")
         dmonth (dray 1)
@@ -21,27 +15,49 @@
 (defn time-format [time]
   (subs time 0 5))
 
+(defn date-format [date]
+  (let [temp (clojure.string/split date #"\s")
+        day (temp 0)
+        time (temp 1)]
+    (str (day-format day) " " (time-format time))))
+
 (defn get-user [username password]
       (let [digest (encrypt-pw password)
-            usrs (select users (fields :id :password) (where {:username username}))
-	    usr (usrs 0)]
-	(if (= (:password usr) (str digest))
-	  (:id usr)
-	  nil))) 
+            usrs (select users (fields :id :password) (where {:username username}))]
+	(if (empty? usrs)
+	    nil
+	    (let [usr (usrs 0)]
+	      (if (= (:password usr) (str digest))
+	      	  (let [usr (usrs 0)] (:id usr))
+	      :passworderr)))))
 
 (defn new-user [email username password]
       (let [digest (encrypt-pw password)
-      	    id (+ 1 (count (select users)))]
-      	   (insert users (values {:id id, :username username, :password digest, :email email}))))
+      	    id (+ 1 (((select users (aggregate (max :id) :id)) 0) :id))]
+	    (try (insert users (values {:id id, :username username, :password digest, :email email}))
+	    	 (catch Exception e (str "caught exception: " (.getMessage e))))))
 
 (defn reset-password [username] nil) ;;sends an email to the users email
 
 (defn new-post [user nickname title city state content]
-  (let [id (+ 1 (count (select posts)))]
+  (let [id (+ 1 (((select posts (aggregate (max :id) :id)) 0) :id))]
   (insert posts
     (values {:id id, :title title, :city city, :state state,
     	     :content content, :nickname nickname, :user user, :votes 0}))))
 
 (defn get-ad [id] nil) ;;returns link for an ad
 
-(defn get-posts [topic page] (select posts))
+(defn get-recent 
+      ([last n]
+	(let [cnt (((select posts (aggregate (count :*) :count)) 0) :count)]
+	  (select posts (order :created) (offset (- cnt (+ n last))) (limit n))))
+	([n] 
+	   (let [cnt (((select posts (aggregate (count :*) :count)) 0) :count)]
+	     (select posts (order :created) (offset (- cnt n))))))
+
+(defn get-top [a b]
+      (let [cnt (((select posts (aggregate (count :*) :count)) 0) :count)]
+        (select posts (aggregate (order :votes) :votes) (offset (- cnt (+ a b))) (limit b))))
+
+(defn number-posts []
+      (((select posts (aggregate (count :*) :count)) 0) :count))
